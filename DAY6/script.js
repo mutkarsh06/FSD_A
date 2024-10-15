c099000472cc22068ad749ff02a2fccae5494725
mutkarsh06 <mutkarsh701@gmail.com>
2024-10-15 15:38:34 +0530

    Pushing in 15oct

---
+++ DAY6/script.js
@@ -0,0 +1,10 @@
+// let x= confirm("are u sure to logout");
+// alert(x);
+
+// let v = prompt("enter name ","rahul");
+// alert(v);
+
+// let disp=() =>{
+//     console.log("welcome to arrow funtion");s
+// };
+// disp();
